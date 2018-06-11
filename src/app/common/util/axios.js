afdd1286ebda7a5b32e52d0e30e102573a58d836
zhangleinice <963754365@qq.com>
2018-06-11 23:51:25 +0800

    1.0

--- src/app/common/util/axios.js
+++ src/app/common/util/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: '../../../data/Cityinside.json',
-    withCredentials: false,   //跨域请求时是否需要使用凭证
+    baseURL: '',
+    withCredentials: true,   //跨域请求时是否需要使用凭证,是否允许带cookie这些
     responseType: JSON,   //默认
     timeout: 5000,  //请求超时
     headers: {
@@ -11,4 +11,33 @@ const instance = axios.create({
 })
 
 //  Add a request interceptor
-instance.interceptors.request.use
+instance.interceptors.request.use(config => {
+    if(token) {
+        config.headers.Authorization = token 
+    }
+    return config
+}, err => {
+    return Promise.reject(err)
+})
+
+//  Add a response interceptor
+instance.interceptors.response.use(response => {
+    return response
+}, err => {
+    if(err.response) {
+
+    }
+    return err.response.data;
+})
+
+
+const axios = {
+    get(url, params) {
+        return instance.get(url, params)
+    },
+    post(url, params) {
+        return instance.post(url, params)
+    }
+}
+
+export default axios;
