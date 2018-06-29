5ab088e2e45cd7d76ab2bdc75e3a47014f57f646
zhanglei <963754365@qq.com>
2018-06-29 18:06:44 +0800

    middleware

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import router from './app/admin/router/index';
 import {Provider} from 'react-redux';
-// import { createStore, compose, applyMiddleware } from 'redux';
-// import Thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import {hashHistory} from 'react-router';
 // import { syncHistoryWithStore } from 'react-router-redux';
-// import rootReducer from './app/admin/store/reducer';
-// import rootReducer from './app/admin/store/city/reducer';
-import store from './app/admin/store/index';
+import rootReducer from './app/admin/store/reducer';
+import {logger} from './app/admin/store/middleware/index';
+import promiseMiddleware from 'redux-promise';
 
 // compose增强creatStore
 
 
 // 增强history
 // const history = syncHistoryWithStore(hashHistory,store)
+const store = createStore(
+    rootReducer,
+    // 使用applyMiddleware加载middlewares
+    // thunk: 允许dispatch一个函数
+    applyMiddleware(promiseMiddleware, thunk, logger)
+)
 
 // Provider作为根组件，传弟子组件的connect
 ReactDOM.render(
